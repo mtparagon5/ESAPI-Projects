275287e809af218cdb89a64bbe1344d1a434bba4
Matthew Thomas <n083336@eushc.org>
2019-09-16 15:33:38 -0400

    adding gui for constraint check script

---
+++ Projects/ConstraintCheck/Script.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VMS.TPS.Common.Model.API;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public Script()
+        {
+        }
+
+        public void Execute(ScriptContext context, Window window)
+        {
+        }
+    }
+}
