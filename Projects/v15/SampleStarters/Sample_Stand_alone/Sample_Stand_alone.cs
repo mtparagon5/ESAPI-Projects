2a65eb5e6112276708083fbedf72ad4268dc64cc
mtparagon5 <mt.paragon5@gmail.com>
2020-08-05 07:16:58 -0400

    adding samples generated by esapi scripting wizard

---
+++ Projects/v15/SampleStarters/Sample_Stand_alone/Sample_Stand_alone.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Reflection;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+// TODO: Uncomment the following line if the script requires write access.
+// [assembly: ESAPIScript(IsWriteable = true)]
+
+namespace Sample_Stand_alone
+{
+  class Program
+  {
+    [STAThread]
+    static void Main(string[] args)
+    {
+      try
+      {
+        using (Application app = Application.CreateApplication())
+        {
+          Execute(app);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.Error.WriteLine(e.ToString());
+      }
+    }
+    static void Execute(Application app)
+    {
+      // TODO: Add your code here.
+    }
+  }
+}
